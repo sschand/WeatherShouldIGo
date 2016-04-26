f697e4ad6fd169b6d353e2dae0303b5e29175e27
John Doe <johndoe@example.com>
2016-04-26 13:32:05 -0700

    style

--- application/config/database.php
+++ application/config/database.php
@@ -7,15 +7,15 @@ if(ENVIRONMENT == 'production')
 {
 	$db['default']['hostname'] = 'localhost';
 	$db['default']['username'] = 'root';
-	$db['default']['password'] = '';
-	$db['default']['database'] = '';
+	$db['default']['password'] = 'root';
+	$db['default']['database'] = 'windstagram';
 }
 else
 {
 	$db['default']['hostname'] = 'localhost';
 	$db['default']['username'] = 'root';
-	$db['default']['password'] = '';
-	$db['default']['database'] = '';
+	$db['default']['password'] = 'root';
+	$db['default']['database'] = 'windstagram';
 }
 
 $db['default']['dbdriver'] = 'mysql';
--- application/views/index.php
+++ application/views/index.php
@@ -3,14 +3,40 @@
 <head>
 	<link rel="shortcut icon" type="image/x-icon" href="/weathers.ico" />
 	<title>Weather should I go?</title>
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	<link rel="stylesheet" type="text/css" href="/assets/css/skeleton.css">
 	<link rel="stylesheet" type="text/css" href="/assets/css/style.css">
 	<script src="/assets/jquery.min.js" type="text/javascript"></script>
 </head>
 <body>
-<div class="other">
-	
-</div>
+	<nav class="navbar navbar-inverse navbar-fixed-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+
+	          <a class="navbar-brand" href="#">Windstagra'm</a>
+	        </div>
+	        <div id="navbar" class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav navbar-right">
+	            <li><a href="#">My Trips</a></li>
+	            <li><a href="#about">Log out</a></li>
+
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </nav>
 
 <div class="main container">
 	<h1>Weather should I go?</h1>
@@ -31,7 +57,7 @@
 	    	</div>
 	    	<div class="four columns">
 	    		<button type="submit" class="button-primary">Submit</button>
-	    	</div>	    
+	    	</div>
 	    </div>
 	</form>
 	<div id="results">
@@ -47,7 +73,7 @@
 
 			// this will be shown while user is waiting for response
 			$('#loading').html("<img src='assets/images/spinner.gif'>");
-			$('#images').html("");	    
+			$('#images').html("");
 
 			$.post($(this).attr('action'), $(this).serialize(), function(res) {
 		    	var images_string = "";
@@ -57,25 +83,25 @@
 		    	if(res.data.length > 0){
 		    		console.log(res.data);
 		    		// console.log(res.data[i].tags.length);
-	    			for (var i = 0; i < res.data.length; i++) {	
+	    			for (var i = 0; i < res.data.length; i++) {
 
-	    			  	// for (var i = 0; i < res.data[i].tags.length; i++) {	
+	    			  	// for (var i = 0; i < res.data[i].tags.length; i++) {
     			  		console.log(res.data[i].tags);
     			  		if(res.data[i].tags.includes(weather)){
     			  			console.log('yes!!');
     			  			images_string +='<img src='+res.data[i].images.low_resolution.url+'>';
-    			  		}	    				
+    			  		}
 			       	}
 			       	if(images_string.length < 1){
 			       		images_string += "NONE";
 			       	}
 		    	}else{
 		    		images_string += "<h2>No images found :(</h2>";
-		    	}			    
+		    	}
 
 		       	// remove loading image
-				$('#loading').html("");		
-				$('#images').html(images_string);	    
+				$('#loading').html("");
+				$('#images').html(images_string);
 			    $('input').val('');
 			}, 'json');
 
