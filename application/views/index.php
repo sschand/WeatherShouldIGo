7050bbfaaf5513a5b6ef91eae289b265783835da
Sharol Chand <sharolchand@gmail.com>
2016-04-27 12:12:36 -0700

    added register dob must be 21 years or older to register

--- application/views/index.php
+++ application/views/index.php
@@ -104,27 +104,22 @@
 					<form id="register_form" class="form-horizontal" action="/login/store_user_register" method=post>
 							<input type="hidden" name="action" value="register">
 							<div >
-								<input id="name" class="col-md-10" name ="name" type="text" placeholder="Full Name" required>
-
+								<input id="name" class="col-md-10" name ="name" type="text" placeholder="Full name" required>
 							</div>
 							<div >
 								<input id="user_name" class="col-sm-10" name="user_name" type="text" placeholder="User name" required>
-
 							</div>
 							<div >
 								<input id="email" class="col-sm-10" name="email" type="email" placeholder="Email" class=emailinput required>
-
 							</div>
 							<div >
 								<input id="password" class="col-sm-10" name="password" type="password" placeholder="Password" minlength="8" required>
-
 							</div>
 							<div >
 								<input id="confirm_password" class="col-sm-10" name="confirm_password" type="password" placeholder="Confirm Password" required >
 							</div>
 							<div >
-								<input id="dob" class="col-sm-10" name="dob" type="text" onfocus="(this.type='date')" id="datepicker" placeholder="Date of Birth" required max="1995-04-29">  <!-- Date - for 21+ hard-coded for now  -->
-
+								<input id="dob" class="col-sm-10" name="dob" type="text" onfocus="(this.type='date')" id="datepicker" placeholder="Date of Birth (must be at least 21 years)" required max="1995-04-29">  <!-- Date - for 21+ hard-coded for now  -->
 							</div>
 							<div class="">
 								<input type="submit" class="btn btn-default" value="Register" id="regbtn" required>
