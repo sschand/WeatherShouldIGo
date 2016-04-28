7fd4b604456446117034f9fce872f2d399ab30da
Howard Jiang <howard.yunghao.jiang@gmail.com>
2016-04-27 18:27:26 -0700

    TEMORARILY REMOVED SUN :[

--- assets/map.js
+++ assets/map.js
@@ -14,17 +14,17 @@
 // }
 
 $(document).ready(function() {
-    $('body').append('<img class="morningSun" src="/assets/images/origWeather/sun.png" style="height: 200px; position: absolute; left: -300px; top: 30%; z-index: 5000">');
-    $('body').append('<div class="coverForLoad" style="position: absolute; top: 0; left: 0; width: 100vw; height: 100vw; z-index: 3000; background: rgba(180,180,180,0.3)"></div>')
-            .queue(function() {
-                $('.morningSun').animate({left:'100%'}, 3000, function() {
-                    $('.morningSun').hide();
-                })
-                .delay(0)
-                .queue(function(){
-                    $('.coverForLoad').hide();
-                });
-            })
+    // $('body').append('<img class="morningSun" src="/assets/images/origWeather/sun.png" style="height: 200px; position: absolute; left: -300px; top: 30%; z-index: 5000">');
+    // $('body').append('<div class="coverForLoad" style="position: absolute; top: 0; left: 0; width: 100vw; height: 100vw; z-index: 3000; background: rgba(180,180,180,0.3)"></div>')
+    //         .queue(function() {
+    //             $('.morningSun').animate({left:'100%'}, 3000, function() {
+    //                 $('.morningSun').hide();
+    //             })
+    //             .delay(0)
+    //             .queue(function(){
+    //                 $('.coverForLoad').hide();
+    //             });
+    //         })
     // $('nav').css('backgroundColor', 'rgba(180, 180, 180, 0.76)');
     $('nav.navbar.navbar-custom.navbar-fixed-top').css('backgroundColor', 'rgba(180,180,180,0.0)');
 
@@ -241,7 +241,7 @@ function getInstagram(name, weather) {
     weather ="fog";
   } else if(weather == "Drizzle"){
     weather ="rain";
-  } 
+  }
 
     // this will be shown while user is waiting for response
     $('#loading').html("<img src='assets/images/loading.gif'>");
