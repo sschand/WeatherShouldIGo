d5987d6ec1fdbfd10e4ecaa51fdf1b0d4b38d98c
John Doe <johndoe@example.com>
2016-04-28 16:15:25 -0700

    change name

---
+++ application/controllers/login.php
@@ -0,0 +1,86 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+ class Login extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		//$this->output->enable_profiler();
+		date_default_timezone_set('America/Los_Angeles');
+		$this->load->model('user');
+
+		$this->load->library("form_validation");
+		$this->load->helper('form');
+	}
+
+	public function index()
+	{
+    $this->load->view('index');
+	}
+
+	// Register user, validation is done with Boostrap on client side
+	public function store_user_register(){
+		$info = $this->input->post();
+		$user_id = $this->User->store_user_register($info);
+		$this->session->set_userdata('user_id', $user_id);
+		redirect(base_url().'login/get_user');
+	}
+
+	//Grab user info and redirect to log in
+	public function get_user(){
+    	$user = $this->User->get_user($this->session->userdata['user_id']);
+		$this->session->set_flashdata('success_register',"You were successfully registered, login to continue <i class='fa fa-flag-checkered' aria-hidden='true'></i>");
+    	$data = array('user'=> $user);
+		redirect(base_url().'');
+	}
+
+	public function logoff(){
+		$this->session->sess_destroy();
+		redirect(base_url().'');
+	}
+
+	public function store_user_login(){
+		$email = $this->session->userdata('user_email');
+
+		$user = $this->User->store_user_login($email);
+
+		$this->session->set_userdata('user_id',$user['user_id']);
+		$this->session->set_userdata('user_name',$user['user_name']);
+		redirect(base_url().'/main');
+	}
+
+	public function validate_user($email, $password){
+		$user = $this->User->store_user_login($email);
+		$response = '';
+
+		if($user && $user['password'] == md5($password)){
+			$this->session->set_userdata('user_id', $user['user_id']);
+			$this->session->set_userdata('user_name', $user['user_name']);
+			$this->session->set_userdata('user_email', $email);
+			$response = "User and email correct";
+		}else {
+			$response =  "Incorrect login";
+		}
+		echo $response;
+	}
+
+
+
+public function logged($name){
+
+	$this->session->set_userdata('city_name',$name);
+	if($this->session->userdata('user_id')){
+		//$this->User->insertCityByName($name,$this->session->userdata('user_id'));
+		$this->load->view('newevent');
+	} else {
+		$this->session->set_flashdata('loggedFail','Must be logged in to plan a trip!');
+		redirect(base_url().'');
+	}
+}
+
+	// public function plan_trip($city){
+	// 	$this->load->view('newevent');
+	// }
+}
+
+//end of main controller
---
+++ application/controllers/trip.php
@@ -0,0 +1,102 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Trip extends CI_Controller {
+    public function __construct(){
+        parent::__construct();
+        // When encountering No Access Control Origin issues
+        //$this->load->library('PHPRequests');
+        $this->load->model('user');
+    }
+    public function index()
+    {
+      $trip = $this->User->get_trip($this->session->userdata('user_id'));
+      $friendTrip = $this->User->friend_trip($this->session->userdata('user_id'));
+      for($i=0;$i < count($friendTrip); $i++)
+      {
+        $peopleCount = $this->User->get_people($friendTrip[$i]['trip_id']);
+        $cityID = $friendTrip[$i]['trip_id'];
+        $this->session->set_userdata($cityID, $peopleCount);
+      }
+
+      $friends = $this->User->add_friend($this->session->userdata('user_id'),$this->session->userdata('display_trip_id'));
+      $this->session->set_userdata('friends', $friends);
+
+      $trip = $this->User->get_trip($this->session->userdata('user_id'));
+      $data = array('triplist'=> $trip, 'friendTripList'=> $friendTrip);
+
+      //var_dump($data);
+      $this->load->view('trip',$data);
+    }
+
+    public function create_trip(){
+      $info = $this->input->post();
+      $this->User->create_trip($this->session->userdata('city_name'),$this->session->userdata('user_id'),$info);
+      redirect(base_url().'trip');
+    }
+
+    public function getTripByid($trip_id){
+      $this->session->set_userdata('display_trip_id', $trip_id);
+      $trip = $this->User->get_trip($this->session->userdata('user_id'));
+      $info = $this->User->getTripByid($trip_id);
+      $friendTrip = $this->User->friend_trip($this->session->userdata('user_id'));
+      $friends = $this->User->add_friend($this->session->userdata('user_id'),$this->session->userdata('display_trip_id'));
+      $this->session->set_userdata('friends', $friends);
+
+      //var_dump($info);
+      $data = array('tripinfo'=>$info,'triplist'=> $trip,'friendTripList'=> $friendTrip);
+      $this->load->view('trip',$data);
+
+    }
+
+
+    public function add_friendToTrip(){
+      $info = $this->input->post('friend_select');
+      //var_dump($info);
+     $this->User->add_friendToTrip($info,$this->session->userdata('display_trip_id'));
+
+     //Twilio Sending Message grabs
+     $friendNumber = $this->User->getPhoneByUserId($info);
+     $trip = $this->User->getTripNameByTripId($this->session->userdata('display_trip_id'));
+     $this->sendMessageForTrip($friendNumber, $trip);
+
+     $this->session->unset_userdata('friends');
+
+       redirect(base_url().'trip/getTripByid/'.$this->session->userdata('display_trip_id'));
+    }
+    protected function sendMessageForTrip($phone, $trip) {
+        //Requires a file set in assets before you can accomplish everything
+        set_include_path(dirname(__FILE__)."/../");
+        require('assets/services/Twilio.php');
+
+        //Account based tokens, Don't touch
+        $account_sid = 'AC0bf0467f0af56fc24371c76da012e428';
+        $auth_token = 'eea66dc4d709d27d2469d6d609af0dbf';
+
+        //Creates new Twilio Service
+        $client = new Services_Twilio($account_sid, $auth_token);
+
+        //Sending message area
+        $message = $client->account->messages->sendMessage(
+            '+14084713857', //Twilio Phone Number (Don't change for now)
+            $phone, //Recipient Phone Number
+            $trip['description'].'... You have been invited to go to '.$trip['city_name'].' On '.$trip['start_date'].' by '.$this->session->userdata('user_name')     //Message to send in the text
+        );
+    }
+
+    public function add_friend_to_list(){
+      //var_dump($this->input->post('user_name'));
+      $info = $this->User->get_user_id($this->input->post('user_name'));
+
+      $this->User->add_friend_to_list($this->session->userdata('user_id'),$info['user_id']);
+      $this->session->unset_userdata('display_trip_id');
+      redirect(base_url().'trip');
+    }
+
+    public function remove_friend($friend_id){
+      $this->User->remove_friend($friend_id);
+      redirect(base_url().'trip/getTripByid/'.$this->session->userdata('display_trip_id'));
+    }
+
+}
+
+//end of main controller
