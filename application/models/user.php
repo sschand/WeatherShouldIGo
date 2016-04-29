d27e9e55f2b5b203d6e7c921f2b57614014ed175
Sharol Chand <sharolchand@gmail.com>
2016-04-29 09:10:34 -0700

    alphabetizes usernames for add friends by user name

--- application/models/user.php
+++ application/models/user.php
@@ -125,7 +125,7 @@ class User extends CI_Model {
       }
 
       function getUsersByUsername($userNotToGet){
-          $query = "SELECT user_name, user_id FROM users where user_id!=? AND user_id not in (SELECT friends.user_id as friend_id from users JOIN friends on friends.friend_id=users.user_id JOIN users as user_friends on friends.user_id = user_friends.user_id WHERE users.user_id = ?)";
+          $query = "SELECT user_name, user_id FROM users WHERE user_id!=? AND user_id NOT IN (SELECT friends.user_id AS friend_id FROM users JOIN friends ON friends.friend_id=users.user_id JOIN users AS user_friends ON friends.user_id = user_friends.user_id WHERE users.user_id = ?) ORDER BY user_name ASC";
           $values = array($userNotToGet, $userNotToGet);
 
           return $this->db->query($query, $values)->result_array();
