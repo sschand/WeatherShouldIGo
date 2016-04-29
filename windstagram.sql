51829c8147aea5cd4dd1727b9c24270682d44469
John Doe <johndoe@example.com>
2016-04-28 18:40:50 -0700

    sql added

--- windstagram.sql
+++ windstagram.sql
@@ -27,7 +27,7 @@ CREATE TABLE `friends` (
   `friend_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,6 +36,7 @@ CREATE TABLE `friends` (
 
 LOCK TABLES `friends` WRITE;
 /*!40000 ALTER TABLE `friends` DISABLE KEYS */;
+INSERT INTO `friends` VALUES (1,4,1),(2,1,4),(3,2,1),(4,1,2),(5,3,1),(6,1,3),(7,NULL,1),(8,1,NULL);
 /*!40000 ALTER TABLE `friends` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,7 +55,7 @@ CREATE TABLE `trips` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`trip_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -63,6 +64,7 @@ CREATE TABLE `trips` (
 
 LOCK TABLES `trips` WRITE;
 /*!40000 ALTER TABLE `trips` DISABLE KEYS */;
+INSERT INTO `trips` VALUES (2,'denver','Bring your boards and warm clothes, it is gonna be cold','2016-05-10 00:00:00','2016-04-29 01:12:39',NULL);
 /*!40000 ALTER TABLE `trips` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -82,7 +84,7 @@ CREATE TABLE `trips_users` (
   KEY `fk_users_has_trips_users_idx` (`user_id`),
   CONSTRAINT `fk_users_has_trips_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_users_has_trips_trips1` FOREIGN KEY (`trip_id`) REFERENCES `trips` (`trip_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -91,6 +93,7 @@ CREATE TABLE `trips_users` (
 
 LOCK TABLES `trips_users` WRITE;
 /*!40000 ALTER TABLE `trips_users` DISABLE KEYS */;
+INSERT INTO `trips_users` VALUES (1,2,2),(3,2,4),(2,2,5);
 /*!40000 ALTER TABLE `trips_users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -112,7 +115,7 @@ CREATE TABLE `users` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -121,6 +124,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'pegah keshavarz','pegah','pegah@reviveapp.com','ec4e656981fb30f38a5c3dad99326c9d','1987-01-15 00:00:00','4158252361','2016-04-28 17:56:17','2016-04-28 17:56:17'),(2,'howard jiang','howard','me@howardjiang.com','6a204bd89f3c8348afd5c77c717a097a','1989-12-12 00:00:00','4088988860','2016-04-28 18:01:53','2016-04-28 18:01:53'),(3,'vadim','vadim','vkutuyev@gmail.com','5f4dcc3b5aa765d61d8327deb882cf99','1986-01-01 00:00:00','9492927463','2016-04-28 18:03:11','2016-04-28 18:03:11'),(4,'billy','billy','billy@gmail.com','6a204bd89f3c8348afd5c77c717a097a','1990-12-12 00:00:00','415555555','2016-04-28 18:03:46','2016-04-28 18:03:46'),(5,'ben aflec','ben','ben@gmail.com','6a204bd89f3c8348afd5c77c717a097a','1980-12-12 00:00:00','555555555','2016-04-28 18:04:15','2016-04-28 18:04:15'),(6,'alis lia','alis','alis@gmail.com','6a204bd89f3c8348afd5c77c717a097a','1980-12-12 00:00:00','444444444','2016-04-28 18:04:48','2016-04-28 18:04:48'),(7,'mike choi','mike','mike@gmail.com','6a204bd89f3c8348afd5c77c717a097a','1980-12-12 00:00:00','111111111','2016-04-28 18:05:19','2016-04-28 18:05:19'),(8,'oscar','oscar','oscar@gmail.com','6a204bd89f3c8348afd5c77c717a097a','1980-12-12 00:00:00','222222222','2016-04-28 18:05:48','2016-04-28 18:05:48'),(9,'jay','jay','jay@gmail.com','6a204bd89f3c8348afd5c77c717a097a','1988-12-12 00:00:00','333333333','2016-04-28 18:06:21','2016-04-28 18:06:21');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -133,4 +137,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-04-28 17:35:00
+-- Dump completed on 2016-04-28 18:40:12
